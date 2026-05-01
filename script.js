5c65e9512fbb273324a51dad6f8a8db8a3e4570a
Remy Robert <23296rr@hvhs.school.nz>
2026-05-01 12:31:38 +1200

    added safe read, listening, and an object thing

--- script.js
+++ script.js
@@ -17,26 +17,67 @@ const HTML_OUTPUT = document.getElementById("databaseOutput");
 // The ref('/') part tells the operation to write to the base level of the database "/"
 // This means it replaces the whole database with message:Hello World
 /**************************************************************/
-function helloWorld(){
-  console.log("Running helloWorld()")
+function bingbong(){
+  console.log("Running bingbong()")
   firebase.database().ref('/').set(
     {
-      message: 'bye'
+      pinthatball: {
+        users: {
+          Dima: {
+            highscore: 70000,
+            lowscore: 1000,
+          },
+          Toby: {
+            highscore: 7000,
+            lowscore: 0,
+          },
+          Yannik: {
+            highscore: 1000000,
+            lowscore: 50000,
+          },
+          Jacob: {
+            hightscore: 20000000,
+            lowscore: 20000000,
+          },
+          Savin: {
+            hightscore: 1000,
+            lowscore: 100000000,
+          },
+        }
+      }
     }
   )
 }
 
 function read() {
   console.log("Begun reading")
-  firebase.database().ref('/message').once('value', DO_THIS)
+  firebase.database().ref('/pinthatball/users/jacob/lowscore').once('value', DO_THIS_UNSAFE)
 }
-  
+
+function saferead() {
+  console.log("Begun safe reading")
+  firebase.database().ref('/bingbong/pinthatball/users/jacob/lowscore').once('value', DO_THIS, fb_readError)
+}
+
+function listening() {
+  console.log("Begun listening")
+  firebase.database().ref('/bingbong').on('value', DO_THIS, fb_readError)
+}
+
+function DO_THIS_UNSAFE(snapshot) {
+  console.log(snapshot.val())
+}
+
 function DO_THIS(snapshot) {
-  console.log("do this is doing this")
   var dbData = snapshot.val();
   if (dbData == null) {
     console.log('There was no record when trying to read the message');
   } else {
     console.log("The message is: " + dbData)
   }
+}
+
+function fb_readError(error) {
+  console.log("There was an error reading the message");
+  console.error(error);
 }
